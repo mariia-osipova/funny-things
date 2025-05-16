056d93b5739b54f23e5382725437ac279bd8b04a
maria-osipova <maryosipovaa@gmail.com>
2025-05-15 21:35:21 -0300

    initial commit

---
+++ book-problems/weird-algorithm.py
@@ -0,0 +1,12 @@
+n = int(input('please put integer: '))
+list = [n]
+
+while n != 1:
+    if n % 2 == 0:
+        n = n // 2
+        list.append(n)
+    else:
+        n = n * 3 + 1
+        list.append(n)
+
+print(*list) #unpucking
